b0fea2bdfd0194568eeed31231fefa3ea12e8b68
Ken <keneithatillo@gmail.com>
2024-10-07 15:37:04 +0000

    Lab02 bash script complete

---
+++ lab02/lab02_exercise2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# this is a comment
+
+# for loop to count to 10
+for c in {1..5}; do
+	echo "Count: $c"
+
+	# if does not use == it uses -eq
+	# note the spaces around if [ ]
+	if [ $c -eq 3 ]; then
+		echo "found the third item"
+	fi
+done
+
+# how do we pass parameters from the command line
+# into this bash script. 
+# we use the notation $1, $2 etc to represent
+# the first, second etc parameter into this script
+if [ -z $1 ]; then
+	echo "You didn't pass any paraemters to $0"
+else
+	echo "You passed in $1 to $0"
+fi
+
+# heres a brand new command: 
+# it calls ps -ef, then pipes it into word counter
+# then stores the result in ct
+ct=$(ps -ef | wc -l)
+echo "There are $ct processes running on this machine"
+
+timestamp=$(date -u +"%Y-%m-%d %H:%M:%S")
+if [ $1 -gt $ct ]; then
+	echo "Maximum number of processes exceeded $timestamp" >> log.txt
+else
+	echo "Maximum number of processes NOT exceeded $timestamp" >> log.txt
+fi
